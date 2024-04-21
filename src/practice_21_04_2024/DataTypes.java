cefb76192808dae944ccd8c173af5f3a53531a08
NehallBaig <nehallbaig@gmail.com>
2024-04-21 17:18:45 +0500

    explain git commands and local variable

---
+++ src/practice_21_04_2024/DataTypes.java
@@ -0,0 +1,13 @@
+package practice_21_04_2024;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        // local variable
+        // declare and initialize
+        int num = 10;
+        System.out.println("num is "+num);
+
+    }
+}
